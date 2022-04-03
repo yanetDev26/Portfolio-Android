60ad500fa53c936ded420be50447fb49458bdcaa
rodriguezyanet <79538736+rodriguezyanet@users.noreply.github.com>
2022-04-03 12:26:58 -0300

    first changes

--- app/src/main/java/com/example/mypresentation/MainActivity.kt
+++ app/src/main/java/com/example/mypresentation/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.mypresentation
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,64 +33,79 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mypresentation.ui.theme.MyPresentationTheme
 import kotlinx.coroutines.delay
+import java.lang.reflect.Type
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyPresentationTheme {
-                navigation()
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Navigation()
+                }
             }
         }
     }
 }
 
 @Composable
-fun navigation() {
-
+fun Navigation() {
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = "splash_screen"
-    ) {
-
+    NavHost(navController = navController,
+        startDestination = "splash_screen") {
         composable("splash_screen") {
             SplashScreen(navController = navController)
         }
 
         composable("main_screen") {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Main Screen", fontSize = 30.sp, fontWeight = FontWeight.Bold)
-            }
+            MainSreen()
+        }
+    }
+}
 
+@Preview
+@Composable
+fun MainSreen() {
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+
+        Text("I'm Yanet DevMobile")
+
+        Box(contentAlignment = Alignment.TopCenter,
+            modifier = Modifier.fillMaxSize().padding(top = 8.dp)) {
+            Image(painter = painterResource(id = R.drawable.perfil),
+                contentDescription = "Splash",
+                modifier = Modifier.width(100.dp).height(100.dp))
         }
     }
 }
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
 
+    // AnimationEffect
     LaunchedEffect(key1 = true) {
-        delay(5000L)
+        scale.animateTo(
+            targetValue = 0.7f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(4f).getInterpolation(it)
+                })
+        )
+        delay(3000L)
         navController.navigate("main_screen")
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.splash),
-            "",
-            modifier = Modifier
-                .width(200.dp)
-                .height(200.dp)
-        )
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Splash",
+            modifier = Modifier.scale(scale.value).width(900.dp).height(900.dp))
     }
 }
--- app/src/main/java/com/example/mypresentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mypresentation/ui/theme/Theme.kt
@@ -1,32 +1,21 @@
 package com.example.mypresentation.ui.theme
 
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Orange,
-    primaryVariant = Navy,
-    secondary = Yellow
+    primary = Navy,
+    primaryVariant = Red,
+    secondary = Orange
 )
 
 private val LightColorPalette = lightColors(
-    primary = Red,
-    primaryVariant = Navy,
-    secondary = Yellow
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Yellow,
+    primaryVariant = Orange,
+    secondary = Red
 )
 
 @Composable
