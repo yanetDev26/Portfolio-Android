b78f865b40827d430d0f5ffd8c1dd76844566082
rodriguezyanet <79538736+rodriguezyanet@users.noreply.github.com>
2022-04-08 00:07:40 -0300

    new logo

--- app/src/main/java/com/example/mypresentation/MainActivity.kt
+++ app/src/main/java/com/example/mypresentation/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.mypresentation
 
-import android.graphics.Paint
 import android.os.Bundle
-import android.view.WindowInsets
 import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,30 +8,28 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mypresentation.ui.theme.MyPresentationTheme
 import kotlinx.coroutines.delay
-import java.lang.reflect.Type
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +53,6 @@ fun Navigation() {
         composable("splash_screen") {
             SplashScreen(navController = navController)
         }
-
         composable("main_screen") {
             MainSreen()
         }
@@ -67,8 +62,14 @@ fun Navigation() {
 @Preview
 @Composable
 fun MainSreen() {
+    LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Box(contentAlignment = Alignment.TopCenter,
+            modifier = Modifier.fillMaxSize().padding(top = 8.dp)) {
+            Image(painter = painterResource(id = R.drawable.banner),
+                contentDescription = "Banner",
+                modifier = Modifier.fillMaxSize())
+        }
 
         Text("I'm Yanet DevMobile")
 
@@ -77,6 +78,15 @@ fun MainSreen() {
             Image(painter = painterResource(id = R.drawable.perfil),
                 contentDescription = "Splash",
                 modifier = Modifier.width(100.dp).height(100.dp))
+        }
+
+        Column {
+            Box(contentAlignment = Alignment.TopCenter,
+                modifier = Modifier.fillMaxSize().padding(top = 8.dp)) {
+                Image(painter = painterResource(id = R.drawable.linkedin),
+                    contentDescription = "Splash",
+                    modifier = Modifier.width(10.dp).height(10.dp))
+            }
         }
     }
 }
@@ -103,8 +113,10 @@ fun SplashScreen(navController: NavController) {
 
     // Image
     Box(contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()) {
-        Image(painter = painterResource(id = R.drawable.logo),
+        modifier = Modifier.fillMaxSize().background(
+            colorResource(id = R.color.lightBlue)
+        )) {
+        Image(painter = painterResource(id = R.drawable.banner),
             contentDescription = "Splash",
             modifier = Modifier.scale(scale.value).width(900.dp).height(900.dp))
     }
