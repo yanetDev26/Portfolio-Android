cf67ffd448fa44c74aa444f9858998a24087168a
@yanetrodriguez26 <yanet-rodriguez@outlook.com.ar>
2023-05-19 11:49:09 -0300

    Create Kotlin Project with Material

--- app/src/main/java/com/example/mypresentation/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mypresentation.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Orange = Color(0xFFFDAF75)
-val Red = Color(0xFFF24A72)
-val Navy = Color(0xFF333C83)
-val Yellow = Color(0xFFEAEA7F)
